2a8711f787de0238a736aaa2ad3f5736ff709c47
ahmedfahad04 <bsse1204@gmail.com>
2024-04-17 10:59:31 +0600

    added basic controller and module

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Delete, Get, Param, Patch, Post, Req } from "@nestjs/common";
+import { Request } from "express";
 
-@Controller()
+@Controller('/product')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+    @Get()
+    getProduct() {
+        return { name: "Bottle", amount: '10 pcs' }
+    }
+
+    @Post()
+    addProduct(@Req() req: Request) {
+        return req.body
+    }
+
+    @Get('/:productId')
+    getSingleProduct(@Param() params: { productId: Number }) {
+        return params
+    }
+
+    @Delete('/:productId')
+    removeProduct(@Param() params: { productId: Number }) {
+        return params
+    }
+
+    @Patch('/:productId')
+    updateProduct(@Param() params: { productId: Number }) {
+        return params
+    }
 }
